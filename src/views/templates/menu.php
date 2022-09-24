f510b14f83fd9523b66e5b18b2d9a21cfc3845de
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-09-23 23:46:31 -0300

    beguines

---
+++ src/views/templates/menu.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="logo">SISCOPI</div>
+    <div class="toggle-user-menu"></div>
+    <ul class="user-menu">
+        <li><a href="#">Sair</a></li>
+    </ul>
+</header>
---
+++ src/views/templates/sidebar.php
@@ -0,0 +1,9 @@
+<nav>
+    <a href="#" class="home-icon"></a>
+    <ul class="nav-menu">
+        <li><a href="#">Registrar Venda</a></li>
+        <li><a href="#">Clientes</a></li>
+        <li><a href="#">Produtos</a></li>
+        <li><a href="#">Fornecedor</a></li>
+    </ul>
+</nav>
