4c4eee897d2764fcf206f5fb85c4cef2382d1e6c
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-09-25 00:40:49 -0300

    exceptions

---
+++ src/exceptions/ValidationException.php
@@ -0,0 +1,17 @@
+<?php
+
+class ValidationException extends AppException{
+    private $erros = [];
+    public function __construct($erros = [], $message = 'Erros de validação.', $code = 0, $previous = null){
+        parent::__construct($message, $code, $previous);
+        $this->erros = $erros;
+    }
+
+    public function getErros(){
+        return $this->erros;
+    }
+
+    public function get($att){
+        return $this->erros[$att];
+    }
+}
