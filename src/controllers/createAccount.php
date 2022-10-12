9e8d140ac07221a74e89c813f18516e1736681e1
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-10-11 23:16:20 -0300

    user

--- src/controllers/createAccount.php
+++ src/controllers/createAccount.php
@@ -4,9 +4,8 @@ $dados['id'] = '';
 $dados['username'] = filter_var($_POST['create_username'], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_LOW);
 $dados['email'] = filter_var($_POST['create_email'], FILTER_SANITIZE_EMAIL);
 $dados['password'] = password_hash($_POST['create_password'], PASSWORD_DEFAULT);
+$dados['permissao'] = 1;
 $dados['create_time'] = date('Y-m-d H:i:s');
-$dados['last_login'] = null;
-$dados['status'] = 1;
 
 try{
     $cadastrar = User::createAccount($dados);
--- src/models/User.php
+++ src/models/User.php
@@ -4,19 +4,29 @@ class User extends Model{
     protected static $tableName = 'user';
     protected static $columns = [
         "id",
+        "username",
+        "email",
+        "password",
+        "permissao",
+        "create_time"
     ];
 
     public static function createAccount($dados = []){
         $conn = Database::getConnection();
         $sql = "INSERT INTO " . self::$tableName . " ("
-            . implode(",", self::$columns). ") VALUES (?,?)";
+            . implode(",", self::$columns). ") VALUES (?,?,?,?,?,?)";
         
         $stmt = $conn->prepare($sql);
         $params = [
-            $dados[''],
+            $dados['id'],
+            $dados['username'],
+            $dados['email'],
+            $dados['password'],
+            $dados['permissao'],
+            $dados['create_time'],
         ];
 
-        $stmt->bind_param("i",...$params);
+        $stmt->bind_param("isssis",...$params);
         if($stmt->execute()){
             addSuccessMsg('Conta criada com sucesso!');
             unset($dados);
