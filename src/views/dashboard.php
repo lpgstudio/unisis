cbf8e89db9034a17cf0842e330423aa72ace0451
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-10-19 23:31:00 -0300

    validação

--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -8,14 +8,14 @@
     <div class="dash-cards-produtos">
         <div class="card">
             <h3>Produtos Cadastrados</h3>
-            <p><?php echo count($produtos); ?></p>
+            <p><?php if(isset($produtos)){echo count($produtos);}else{echo "0";} ?></p>
         </div>
         <div class="card">
             <h3>Produtos Top 5</h3>
         </div>
         <div class="card">
             <h3>Produtos baixo estoque</h3>
-            <p><?php echo count($estoque); ?></p>
+            <p><?php if(isset($estoque)){echo count($estoque);}else{echo "0";} ?></p>
         </div>
         <div class="card">
             <h3>Produtos Vencendo</h3>
@@ -25,7 +25,7 @@
     <div class="dash-cards-clientes">
         <div class="card">
             <h3>Clientes cadastrados</h3>
-            <p><?php echo count($clientes); ?></p>
+            <p><?php if(isset($clientes)){echo count($clientes);}else{echo "0";} ?></p>
         </div>
         <div class="card">
             <h3>Top 5</h3>
