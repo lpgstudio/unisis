b221e9d3e6882be0be81bf3229d9203850582337
Thiago de Paula <thiago_newdesign@hotmail.com>
2022-11-13 17:09:52 -0300

    info

--- src/controllers/dashboard.php
+++ src/controllers/dashboard.php
@@ -10,9 +10,29 @@ $produtos = Produto::getAll($user_id);
 $estoque = Produto::getBaixoEstoque($user_id);
 $clientes = Cliente::getAll($user_id);
 
+$totalCusto = 0;
+$totalVenda = 0;
+$totalLucro = 0;
+$totalItens = 0;
+
+foreach ($produtos as $produto){
+    $totalProd = floatval($produto['valor_custo']) * floatval($produto['estoque']);
+    $totalVend = floatval($produto['valor_venda']) * floatval($produto['estoque']);
+    $totalCusto += $totalProd;
+    $totalVenda += $totalVend;
+
+    $totalItens += floatval($produto['estoque']);
+}
+
+$totalLucro = $totalVenda - $totalCusto;
+
 loadTemplateView('dashboard', [
     'nome' => $nome,
     'produtos' => $produtos,
     'estoque' => $estoque,
-    'clientes' => $clientes
+    'clientes' => $clientes,
+    'custo' => $totalCusto,
+    'venda' => $totalVenda,
+    'lucro' => $totalLucro,
+    'qntEstoque' => $totalItens,
 ]);
--- src/views/dashboard.php
+++ src/views/dashboard.php
@@ -8,8 +8,28 @@
     
     <div class="dash-cards-produtos">
         <div class="card">
-            <h3>Produtos Cadastrados</h3>
-            <p><?php if(isset($produtos)){echo count($produtos);}else{echo "0";} ?></p>
+            <h3>Produtos</h3>
+            <p> Cadastrados: <?php if(isset($produtos)){echo count($produtos);}else{echo "0";} ?></p>
+            <hr>
+            <p> Total de itens: <?php if(isset($qntEstoque)){echo $qntEstoque;}else{echo "0";} ?></p>
+            <hr>
+            <p> 
+                Valor em custo: R$ <?php if(isset($custo)){
+                    echo number_format(htmlentities($custo,ENT_QUOTES), 2, ',', '.');
+                    }else{echo "0,00";} ?>
+            </p>
+            <hr>
+            <p> 
+                Valor em venda: R$ <?php if(isset($venda)){
+                    echo number_format(htmlentities($venda,ENT_QUOTES), 2, ',', '.');
+                    }else{echo "0,00";} ?>
+            </p>
+            <hr>
+            <p> 
+                Lucro estimado: R$ <?php if(isset($lucro)){
+                    echo number_format(htmlentities($lucro,ENT_QUOTES), 2, ',', '.');
+                    }else{echo "0,00";} ?>
+            </p>
         </div>
         <div class="card">
             <h3>Produtos Top 5</h3>
